0f0fecc8108cb22403cd578c449fc0b9c9cde0e2
Sujin Choi <hissujinc@gmail.com>
2023-12-10 18:18:45 +0900

    게시글,게시글 자세히보기,  프로필 생성

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import PostList from '../../components/PostList';
+
+export default function Home() {
+    return (
+        <div>
+            <Header />
+            <PostList />
+            <Footer />
+        </div>
+    );
+}
---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,15 @@
+import Footer from '../../components/Footer';
+import Header from '../../components/Header';
+import Profile from '../../components/Profile';
+import PostList from '../../components/PostList';
+
+export default function ProfilePage() {
+    return (
+        <>
+            <Header />
+            <Profile />
+            <PostList hasNavigation={false} />
+            <Footer />
+        </>
+    );
+}
